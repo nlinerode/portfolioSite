1c257debfa6d5ea696c2e09270affbc89c8f5f3e
Nathan Linerode <nlinerode2@gmail.com>
2017-08-23 13:02:49 -0400

    fixed button to not submit, added message for it

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 var header = document.querySelector('header');
+var submitBtn = document.querySelector('button');
+var btnMessage = document.querySelector('.btnMessage');
+
+// submitBtn.addEventListener('mouseenter', btnMessageAdd);
+// submitBtn.addEventListener('mouseleave', btnMessageDelete);
+// submitBtn.addEventListener('click', btnMessageAdd);
+
 window.addEventListener('scroll', function(){
         if (window.pageYOffset > 50){
             header.classList.add('scrollNav');
@@ -6,6 +13,38 @@ window.addEventListener('scroll', function(){
             header.classList.remove('scrollNav');
         }
     });
+
+
+// function btnMessageAdd(){
+//         btnMessage.innerHTML= "<p>Sorry, this won't send anything yet!</p>"; 
+// }
+// function btnMessageDelete(){
+//     btnMessage.textContent= "";
+// }
+
+function isTouchDevice() {
+    return 'ontouchstart' in window || (navigator.maxTouchPoints > 0) || (navigator.maxTouchPoints > 0);
+}
+
+
+jQuery(document).ready(function($) {
+    if (isTouchDevice()) {
+        $(".projectImg").flip({
+            axis: 'y',
+            trigger: 'click'
+        });
+        // btnMessageAdd();
+    } else {
+        $(".projectImg").flip({
+            axis: 'y',
+            trigger: 'hover'
+        });
+        // btnMessageAdd();
+        // btnMessageDelete();
+    }
+});
+
+
 
 // $(".projectImg").flip({
 //     axis:"y",
@@ -35,19 +74,3 @@ window.addEventListener('scroll', function(){
 //     });
 // });
 // 
-function isTouchDevice() {
-    return 'ontouchstart' in window || (navigator.maxTouchPoints > 0) || (navigator.maxTouchPoints > 0);
-}
-jQuery(document).ready(function($) {
-if (isTouchDevice()) {
-    $(".projectImg").flip({
-        axis: 'y',
-        trigger: 'click'
-    });
-} else {
-$(".projectImg").flip({
-    axis: 'y',
-    trigger: 'hover'
-    });
-}
-});
