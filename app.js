a0dead7e9177a36998fa3e754479245a4e2f641d
Nathan Linerode <nlinerode2@gmail.com>
2017-11-29 12:13:18 -0500

    add style changes, showcase image size, form spacing, bg colors, header color

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express'),
+    app = express(),
+    nodemailer = require('nodemailer');
+
+
+var router = express.Router();
+
+// app.listen(5500, )
+console.log('hello world');
